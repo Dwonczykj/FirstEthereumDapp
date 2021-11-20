ad1b1f5ae2f575d7ecab49a40ec605dbd548c359
Joe Dwonczyk <JDwonczyk@gmail.com>
2021-11-20 10:12:42 +0400

    Update the TodoList smart contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,23 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0; //State variable, actually written to the blockchain
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out trollTheTrolls.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
