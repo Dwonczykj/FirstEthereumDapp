bf242e9da86b9e4ef257dbebe0182481fa490dc2
Joe Dwonczyk <JDwonczyk@gmail.com>
2021-11-22 11:02:02 +0400

    Add createTask event to the smart contract so that we can listed to new tasks that are created from client

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,6 +11,8 @@ contract TodoList {
 
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(uint id, string content, bool completed);
+
     constructor() public {
         createTask("Check out trollTheTrolls.com");
     }
@@ -18,6 +20,7 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 
 
