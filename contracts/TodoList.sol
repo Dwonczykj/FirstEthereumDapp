02ec070fa460a48ce8ef6ca34befcc42e69a9f88
Joe Dwonczyk <JDwonczyk@gmail.com>
2021-11-22 18:09:23 +0400

    Add task completion code

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
     uint public taskCount = 0; //State variable, actually written to the blockchain
@@ -13,6 +13,8 @@ contract TodoList {
 
     event TaskCreated(uint id, string content, bool completed);
 
+    event TaskCompleted(uint id, bool completed);
+
     constructor() public {
         createTask("Check out trollTheTrolls.com");
     }
@@ -23,5 +25,10 @@ contract TodoList {
         emit TaskCreated(taskCount, _content, false);
     }
 
-
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id]; // This is how you create a task in memory
+        _task.completed = !_task.completed;
+        tasks[_id] = _task; //done this way as the tasks in the array themselves are immutable.
+        emit TaskCompleted(_id, _task.completed);
+    }
 }
--- src/app.js
+++ src/app.js
@@ -169,7 +169,7 @@ App = {
             $newTaskTemplate.find('input')
                 .prop('name', taskId)
                 .prop('checked', taskCompleted)
-            // .on('click', App.toggleCompleted)
+                .on('click', App.toggleCompleted)
 
             // Put the task in the correct list
             if (taskCompleted) {
@@ -183,6 +183,19 @@ App = {
         }
     },
 
+    createTask: async () => {
+        App.setLoading(true)
+        const content = $('#newTask').val()
+        await App.todoList.createTask(content)
+        window.location.reload()
+    },
+
+    toggleCompleted: async (e) => {
+        App.setLoading(true)
+        const taskId = e.target.name
+        await App.todoList.toggleCompleted(taskId)
+        window.location.reload()
+    },
 
     setLoading: (boolean) => {
         App.loading = boolean
